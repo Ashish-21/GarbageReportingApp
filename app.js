500896a6f932006ca33d126e2b4d2df6dee85422
Ashish-21 <ashishchandwani21@gmail.com>
2020-08-16 11:48:32 +0530

    Connecting MongoDB

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
+const url = process.env.CONNECTION_URL;
+mongoose.connect(url, { useNewUrlParser: true });
+var conn = mongoose.connection;
 
 
 
@@ -10,6 +13,17 @@ app.use(express.json());
 
 
 
-app.listen(2000, () => {
+
+
+
+
+
+
+
+conn.on('open', () => {
+  console.log('Connected to MongoDB');
+});
+conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
+app.listen(process.env.SERVER_PORT_NUMBER, () => {
   console.log('Server is Listening');
 });
