9cf7015765fa242f3f0b62e9b2a34deda825060e
Ashish-21 <ashishchandwani21@gmail.com>
2020-08-16 13:20:00 +0530

    setting up model for user

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema();
+const userSchemaModel = new Schema({
+  username: {
+    type: 'String',
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: 'String',
+    required: true,
+    trim: true,
+  },
+  emailId: {
+    type: 'String',
+    required: true,
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model('userModel', userSchemaModel);
