a3f85e1e460656ae26410d33182cd6fc37d2a2fc
Ashish-21 <ashishchandwani21@gmail.com>
2020-08-16 16:04:56 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -4,26 +4,19 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 const url = process.env.CONNECTION_URL;
-mongoose.connect(url, { useNewUrlParser: true });
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
 var conn = mongoose.connection;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const userRouter = require('./routes/userHandler');
+app.use('/userHandler', userRouter);
 conn.on('open', () => {
   console.log('Connected to MongoDB');
 });
-conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+conn.on('error',()=>
+{
+  console.log('Error in Connecting MongoDB');  
+});
+
 app.listen(process.env.SERVER_PORT_NUMBER, () => {
   console.log('Server is Listening');
 });
