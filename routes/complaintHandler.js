5045f18e408a814009a0b3795da384b0b3eb8cc5
Ashish-21 <ashishchandwani21@gmail.com>
2020-08-17 12:53:48 +0530

    Error Handling in Complaint Handler

--- routes/complaintHandler.js
+++ routes/complaintHandler.js
@@ -1,8 +1,10 @@
+/* Complaint Handler handles all the request for handling Garbage Reporting complaint data  */
 const express = require('express');
 const complaintRouter = express.Router();
 const jwt = require('jsonwebtoken');
 const complaintModel = require('../models/complaintModel');
-const multer = require('multer');
+const multer = require('multer'); //Using Multer to store files on Server
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'uploads/');
@@ -13,16 +15,16 @@ const storage = multer.diskStorage({
   },
 });
 const uploadFile = multer({ storage: storage });
-complaintRouter.get('/complaint', (req, res) => {
-  console.log('hello');
-  res.send('hello');
-});
 
+/* Saving the complaint data in DB */
 complaintRouter.post(
   '/savecomplaint',
   authenticateToken,
   uploadFile.single('complaintImage'),
   async (req, res) => {
+    console.log(
+      '[complaintHandler.js] Entering Post Call for Saving Complaint Data in DB'
+    );
     try {
       const complaint = new complaintModel({
         userId: req.userTokenData._id,
@@ -40,12 +42,24 @@ complaintRouter.post(
         });
       });
     } catch (error) {
-      console.log('Error Occurred');
+      console.log(
+        '[complaintHandler.js] Error Occurred in Saving Complaint Data' + error
+      );
+      res.json({
+        message: 'Error Occurred in Saving Complaint Data' + error,
+      });
     }
+    console.log(
+      '[complaintHandler.js] Existing Post Call for Saving Complaint Data in DB'
+    );
   }
 );
 
+/* Changing the status of complaint in DB */
 complaintRouter.put('/changeStatus', authenticateToken, async (req, res) => {
+  console.log(
+    '[complaintHandler.js] Entering update Call for changing status of  Complaint Data in DB'
+  );
   try {
     const query = {
       _id: req.body._id,
@@ -59,18 +73,33 @@ complaintRouter.put('/changeStatus', authenticateToken, async (req, res) => {
       })
       .then((result) => {
         res.status(201).json({
-          message: 'Complaint Registered Successfully',
+          message: 'Status of Complaint changed Successfully',
           updatedComplaint: result,
         });
       });
-  } catch (error) {}
+  } catch (error) {
+    console.log(
+      '[complaintHandler.js] Error Occurred in Changing Status of Complaint' +
+        error
+    );
+    res.json({
+      message: 'Error Occurred in Changing Status of Complaint' + error,
+    });
+  }
+  console.log(
+    '[complaintHandler.js] Existing update Call for changing status of  Complaint Data in DB'
+  );
 });
 
+/* Updating the Complaint Data in DB */
 complaintRouter.put(
   '/updateComplaint',
   authenticateToken,
   uploadFile.single('complaintImage'),
   async (req, res) => {
+    console.log(
+      '[complaintHandler.js] Entering update Call for updating  Complaint Data in DB'
+    );
     try {
       const query = {
         _id: req.body._id,
@@ -87,11 +116,27 @@ complaintRouter.put(
             updatedComplaint: result,
           });
         });
-    } catch (error) {}
+    } catch (error) {
+      console.log(
+        '[complaintHandler.js] Error Occurred in updating data of Complaint' +
+          error
+      );
+      res.json({
+        message: 'Error Occurred in updating data of Complaint' + error,
+      });
+    }
+
+    console.log(
+      '[complaintHandler.js] Existing update Call for changing status of  Complaint Data in DB'
+    );
   }
 );
 
+/* Deleting the complaint from DB  */
 complaintRouter.delete('/deleteComplaint', async (req, res) => {
+  console.log(
+    '[complaintHandler.js] Entering delete Call for deleting Complaint Data in DB'
+  );
   try {
     await complaintModel.findByIdAndRemove(req.body._id).then((result) => {
       res.status(201).json({
@@ -99,9 +144,20 @@ complaintRouter.delete('/deleteComplaint', async (req, res) => {
         deletedComplaint: result,
       });
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(
+      '[complaintHandler.js] Error Occurred in Deleting Complaint' + error
+    );
+    res.json({
+      message: 'Error Occurred in Deleting Complaint' + error,
+    });
+  }
+  console.log(
+    '[complaintHandler.js] Existing delete Call for deleting Complaint Data in DB'
+  );
 });
 
+/* Utitlity to verify Authorization token */
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
